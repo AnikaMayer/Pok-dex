803b58adcd96f59cf2932f75677b092c5c5ebe95
Anika Mayer <mayer.anika@protonmail.com>
2026-08-06 20:29:38 +0200

    feat/ refactor/ fix: function dialog, added new Arrays and Cache Imgs

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const POKEMON_CARDS = document.getElementById("pokemon_box");
+const DETAILS_DIALOG = document.getElementById("details_dialog");
 const POKEAPI = "https://pokeapi.co/api/v2";
+const POKEMON_URL = [];
+
+const ALL_INFO = [];
+const IMG_CACHE = {};
 const ALL_POKEMON = [];
 const POKEMON_DETAILS = [];
+const EVO_CHAIN = [];
+
 let searchedPokemon = [];
 
 async function init() {
     await getData();
     await getPkmInfo();
+    console.log(ALL_INFO);
+    console.log(ALL_POKEMON);
     console.log(POKEMON_DETAILS);
+    console.log(IMG_CACHE);
+
+    await getMoreDetails();
 }
 
 async function getData() {
@@ -15,36 +27,134 @@ async function getData() {
     const responseFromJson = await response.json();
 
     for (let j = 0; j < responseFromJson.results.length; j++) {
-        ALL_POKEMON.push(responseFromJson.results[j]);
+        POKEMON_URL.push(responseFromJson.results[j].url);
     }
 }
 
 async function getPkmInfo() {
-    for (const pokemon of ALL_POKEMON) {
-        const response = await fetch(`${pokemon.url}`);
-        const responseFromJson = await response.json();
-        POKEMON_DETAILS.push(responseFromJson);
+    for (const pkmUrl of POKEMON_URL) {
+        const resp = await fetch(`${pkmUrl}`);
+        const respFromJson = await resp.json();
+        ALL_INFO.push(respFromJson);
 
-        renderPkmCards(POKEMON_DETAILS);
+        const pkmImg = respFromJson.sprites.other["official-artwork"].front_default;
+        const pkmTypes = getTypes(respFromJson);
+
+        const pokemonObject = createPkmObj(respFromJson, pkmImg, pkmTypes);
+        ALL_POKEMON.push(pokemonObject);
+    }
+    renderPkmCards(ALL_POKEMON);
+}
+
+function createPkmObj(respFromJson, pkmImg, pkmTypes) {
+    return {
+        name: respFromJson.name,
+        id: respFromJson.id,
+        img: pkmImg,
+        type: pkmTypes,
+    };
+}
+
+async function getMoreDetails() {
+    for (let index = 0; index < ALL_INFO.length; index++) {
+        const pkmImg = ALL_INFO[index].sprites.other["official-artwork"].front_default;
+        const pkmTypes = getTypes(ALL_INFO[index]);
+        const pkmAblts = getAblty(ALL_INFO[index]);
+        const resp = await fetch(ALL_INFO[index].species.url);
+        const respFromJson = await resp.json();
+        const pkmDescr = getDescr(respFromJson);
+        const pkmCtgry = getCtgry(respFromJson);
+
+        const detailsObj = createDetailsObj(index, pkmDescr, pkmImg, pkmTypes, pkmCtgry, pkmAblts);
+        POKEMON_DETAILS.push(detailsObj);
     }
 }
 
-function renderPkmCards(pokemonList) {
+function getTypes(pkmIndex) {
+    const pkmTypes = [];
+    pkmIndex.types.forEach((pkmType) => pkmTypes.push(pkmType.type.name));
+    return pkmTypes;
+}
+
+function getAblty(pkmIndex) {
+    const pkmAblts = [];
+    pkmIndex.abilities.forEach((pkmAbility) => pkmAblts.push(pkmAbility.ability.name));
+    return pkmAblts;
+}
+
+function getDescr(respFromJson) {
+    return respFromJson.flavor_text_entries.find((element) => element.language.name === "en").flavor_text;
+}
+
+function getCtgry(respFromJson) {
+    return respFromJson.genera.find((element) => element.language.name === "en").genus;
+}
+
+function getStats(statName, index) {
+    return ALL_INFO[index].stats.find((element) => element.stat.name === statName).base_stat;
+}
+
+function createDetailsObj(index, pkmDescr, pkmImg, pkmTypes, pkmCtgry, pkmAblts) {
+    return {
+        description: pkmDescr,
+        name: ALL_INFO[index].name,
+        id: ALL_INFO[index].id,
+        img: pkmImg,
+        type: pkmTypes,
+        height: ALL_INFO[index].height,
+        weight: ALL_INFO[index].weight,
+        category: pkmCtgry,
+        abilities: pkmAblts,
+        stats: {
+            hp: getStats("hp", index),
+            attack: getStats("attack", index),
+            defense: getStats("defense", index),
+            sp_attack: getStats("special-attack", index),
+            sp_defense: getStats("special-defense", index),
+            speed: getStats("speed", index),
+        },
+    };
+}
+
+async function renderPkmCards(pokemonList) {
     POKEMON_CARDS.innerHTML = "";
 
     for (let i = 0; i < pokemonList.length; i++) {
         const singlePokemon = pokemonList[i];
-        const pkmImg = singlePokemon.sprites.other["official-artwork"].front_default;
-        POKEMON_CARDS.innerHTML += renderPkmCardsTemplate(singlePokemon, i, pkmImg);
+        POKEMON_CARDS.innerHTML += renderPkmCardsTemplate(singlePokemon, i);
 
+        await loadImg(singlePokemon, i);
         renderTypes(singlePokemon, i);
     }
+}
+
+function getImg(singlePokemon, i) {
+    return new Promise((resolve, reject) => {
+        if (IMG_CACHE[singlePokemon.id]) {
+            resolve(IMG_CACHE[singlePokemon.id]);
+            return;
+        }
+
+        const img = new Image();
+        img.src = `${singlePokemon.img}`;
+        img.onload = () => {
+            IMG_CACHE[singlePokemon.id] = img;
+            resolve(img);
+        };
+        img.onerror = reject;
+    });
+}
+
+async function loadImg(singlePokemon, i) {
+    const pkmImgRef = document.getElementById(`pkm_img_${i}`);
+    const pkmImgLoad = await getImg(singlePokemon, i);
+    pkmImgRef.appendChild(pkmImgLoad);
 }
 
 function renderTypes(singlePokemon, i) {
     const pkmTypesRef = document.getElementById(`pkm_type_${i}`);
 
-    for (let y = 0; y < singlePokemon.types.length; y++) {
+    for (let y = 0; y < singlePokemon.type.length; y++) {
         pkmTypesRef.innerHTML += renderTypesTemplate(singlePokemon, y);
     }
 }
@@ -55,4 +165,30 @@ function capitalizeLetter(index) {
 
 function padNumber(index) {
     return index.toString().padStart(5, "0");
+}
+
+function openDialog(i) {
+    // console.log(singlePokemon);
+    const singlePokemon = ALL_POKEMON[i];
+
+    DETAILS_DIALOG.showModal();
+    DETAILS_DIALOG.innerHTML = dialogTemplate(i);
+    DETAILS_DIALOG.classList.add("opened");
+    loadDialogImg(singlePokemon, i);
+    getMoreDetails();
+}
+
+async function loadDialogImg(singlePokemon, i) {
+    const dialogImgRef = document.getElementById(`dialog_img_${i}`);
+    const pkmImgLoad = await getImg(singlePokemon, i);
+    dialogImgRef.appendChild(pkmImgLoad);
+}
+
+function bubblingProtection(event) {
+    event.stopPropagation();
+}
+
+function closeDialog() {
+    DETAILS_DIALOG.close();
+    DETAILS_DIALOG.classList.remove("opened");
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,22 +1,78 @@
-function renderPkmCardsTemplate(singlePokemon, i, pkmImg) {
+function renderPkmCardsTemplate(singlePokemon, i) {
     return /*html*/ `
-        <button class="pokemon_card">
-            <div id="pkm_img_${i}" class="pkm_img_box">
-                <img src="${pkmImg}" alt="${singlePokemon.name}">
-            </div>
-            <div>
+        <button aria-label="show Pokemon details" class="pokemon_card" onclick="openDialog(${i})">
+            <div id="pkm_img_${i}" class="pkm_img_box"></div>
+            <div class="pkm_name">
                 <p>Nr. ${padNumber(singlePokemon.id)}</p>
                 <h2>${capitalizeLetter(singlePokemon.name)}</h2>
             </div>
-            <div id="pkm_type_${i}"></div>
+            <div id="pkm_type_${i}" class="pkm_type"></div>
         </button>
     `;
 }
 
 function renderTypesTemplate(singlePokemon, y) {
     return /*html*/ `
-        <div>
-            <p>${capitalizeLetter(singlePokemon.types[y].type.name)}</p>
+        <div class="single_type">
+            <p>${capitalizeLetter(singlePokemon.type[y])}</p>
         </div>
+    `;
+}
+
+function dialogTemplate(i) {
+    return /*html*/ `
+        <section class="dialog_box">
+            <header class="dialog_header">
+                <h2>${capitalizeLetter(POKEMON_DETAILS[i].name)}</h2>
+                <p>Nr. ${padNumber(POKEMON_DETAILS[i].id)}</p>
+            </header>
+            <div class="dialog_content">
+                <div id="dialog_img_${i}"></div>
+                <div class="main_info">
+                    <p class="description">${POKEMON_DETAILS[i].description}</p>
+                    <div>
+                        <button class="male"></button>
+                        <button class="female"></button>
+                    </div>
+                    <div class="attributes">
+                        <ul>
+                            <li>
+                                <span class="attribute_title">Height:</span>
+                                <span class="attribute_value">${POKEMON_DETAILS[i].height}</span>
+                            </li>
+                            <li>
+                                <span class="attribute_title">Weight:</span>
+                                <span class="attribute_value">${POKEMON_DETAILS[i].weight}</span>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <span class="attribute_title">Category:</span>
+                                <span class="attribute_value">${POKEMON_DETAILS[i].category}</span>
+                            </li>
+                            <li>
+                                <span class="attribute_title">Abilities:</span>
+                                <span class="attribute_value">${POKEMON_DETAILS[i].abilities}</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="data">
+                    <div class="data_type">
+                        <h3>Type</h3>
+                    </div>
+                </div>
+                <div class="stats">
+                    <h3>Stats</h3>
+                    <p>HP <span>${POKEMON_DETAILS[i].stats.hp}</span></p>
+                    <p>Attack <span>${POKEMON_DETAILS[i].stats.attack}</span></p>
+                    <p>Defense <span>${POKEMON_DETAILS[i].stats.defense}</span></p>
+                    <p>Sp.-Attack <span>${POKEMON_DETAILS[i].stats.sp_attack}</span></p>
+                    <p>Sp.-Defense <span>${POKEMON_DETAILS[i].stats.sp_defense}</span></p>
+                    <p>Speed <span></span>${POKEMON_DETAILS[i].stats.speed}</p>
+                </div>
+            </div>
+            <footer class="dialog_footer"></footer>
+        </section>
     `;
 }
